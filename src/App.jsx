b683fab73196d21ad22e881feb093b6a9ceb87c9
KamilTomczykDev <kamiltomczyk.dev@gmail.com>
2024-02-18 00:42:49 +0100

    New device access

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { UnitProvider } from "./contexts/unitContext";
 
+
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
